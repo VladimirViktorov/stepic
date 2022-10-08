21af8c40aa797b352b354412fb70d72dbef5f472
Vladimir <72167223+VladimirViktorov@users.noreply.github.com>
2022-10-08 21:14:03 +0300

    add requirements

--- test_main_page.py
+++ test_main_page.py
@@ -22,4 +22,3 @@ def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
     basket_page = BasketPage(browser, browser.current_url)
     basket_page.should_not_be_product_in_basket()
     basket_page.should_be_text_about_empty_basket()
-    
